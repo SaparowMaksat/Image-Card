09808a7e772d8417a158deb019096b7d5283f2c8
Maksat <makezovs@gmail.ru>
2022-09-25 21:48:26 +0300

    refactor code js

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const sliders = document.querySelectorAll('.slide')
+const activeSliders = (active = 0) => {
+	const sliders = document.querySelectorAll('.slide')
 
-const clearActiveClasses = () => {
-	sliders.forEach(slider => {
-		slider.classList.remove('active')
-	})
+	sliders[active].classList.add('active')
+
+	const clearActiveClasses = () => {
+		sliders.forEach(slider => {
+			slider.classList.remove('active')
+		})
+	}
+
+	for (const slider of sliders) {
+		slider.addEventListener('click', () => {
+			clearActiveClasses()
+			slider.classList.add('active')
+		})
+	}
 }
 
-for (const slider of sliders) {
-	slider.addEventListener('click', () => {
-		clearActiveClasses()
-		slider.classList.add('active')
-	})
-}
+activeSliders()
